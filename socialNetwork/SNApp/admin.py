2b46a73b5b7e281b3dd19170385ca23e12be00ed
ilmush <zainukovail@yandex.ru>
2023-09-25 12:31:23 +0300

    models updated

--- socialNetwork/SNApp/admin.py
+++ socialNetwork/SNApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from SNApp.models import User, Post
+from SNApp.models import *
 
 admin.site.register(User)
 admin.site.register(Post)
+admin.site.register(Follow)
+admin.site.register(UserPostRelation)
--- socialNetwork/SNApp/models.py
+++ socialNetwork/SNApp/models.py
@@ -12,6 +12,7 @@ class User(models.Model):
     image = models.ImageField()
     description = models.CharField(max_length=255, blank=True)
     posts = models.ForeignKey('Post', on_delete=models.CASCADE, blank=True)
+    followers = models.ManyToManyField('self', through='Follow', related_name='followers_user', symmetrical=False, blank=True)
 
     @property
     def name(self):
@@ -23,17 +24,42 @@ class User(models.Model):
         if self.user_id is not None:
             return self.user.email
 
+    def follow(self, user):
+        Follow.objects.get_or_create(follower=self, following=user)
+
+    def unfollow(self, user):
+        Follow.objects.filter(follower=self, following=user).delete()
+
+    def is_following(self, user):
+        return self.following.filter(id=user.id).exists()
+
+    def is_followed_by(self, user):
+        return self.followers.filter(id=user.id).exists()
+
     def __str__(self):
         return self.name
 
 
 class Post(models.Model):
-    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    author = models.ForeignKey('User', related_name="author", on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=255, blank=True)
     image = models.ImageField()
+    views = models.ManyToManyField('User', related_name="post_views", through='UserPostRelation')
 
     def __str__(self):
         return self.title
 
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following_set', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='follower_set', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class UserPostRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_notes = models.BooleanField(default=False)
 
